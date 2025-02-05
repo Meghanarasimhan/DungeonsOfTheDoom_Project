052acbdbb57bdefea896fa92d3f5bc5118406106
April Uzzle <anuzzle6@gmail.com>
2025-02-04 23:23:35 -0600

    Update test cases for model classes

--- src/main/java/com/se459/model/DungeonModel.java
+++ src/main/java/com/se459/model/DungeonModel.java
@@ -4,19 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DungeonModel {
-    private final int numFloors;
     private final List<FloorModel> floors;
     private final PlayerModel player;
 
     public DungeonModel() {
-        numFloors = 1;
         floors = new ArrayList<>();
         floors.add(new FloorModel());
         player = new PlayerModel(1, 1);
     }
 
     public int getNumFloors() {
-        return numFloors;
+        return floors.size();
     }
 
     public List<FloorModel> getFloors() {
--- src/test/java/DungeonModelTest.java
+++ src/test/java/DungeonModelTest.java
@@ -20,6 +20,8 @@ public class DungeonModelTest {
         assertEquals("Dungeons should have only one floor in the list", 1, floorModel.getNumRooms());
 
         assertNotNull("Floor should not be null", dungeonModel.getFloors().get(0));
+        assertNotNull("Floors can be accessed individually", dungeonModel.getFloor(0));
+        assertNotNull("Player should not be null", dungeonModel.getPlayer());
     }
 
     @Test
@@ -28,5 +30,15 @@ public class DungeonModelTest {
         assertEquals("Floor should have one room for now", 1, floorModel.getNumRooms());
         assertEquals("Floor should have one room in the list",1,floorModel.getRooms().size());
         assertNotNull("Floor should not be null", floorModel.getRooms().get(0));
+        assertNotNull("Rooms in a floor can be fetched", floorModel.getRoom(0));
+    }
+
+    @Test
+    public void testAddingFloor() {
+        assertEquals("Dungeon should have one floor for now", 1, dungeonModel.getNumFloors());
+
+        dungeonModel.addFloor();
+
+        assertEquals("Dungeon should have two floors now", 2, dungeonModel.getNumFloors());
     }
 }
